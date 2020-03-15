570ee5eed30cb21632a002eea70683d9321ab6ec
RuisongZhou <811437508@qq.com>
2020-03-15 22:51:58 +0800

    init

---
+++ cal_params.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/3/13 12:06 下午
+# @Author  : RuisongZhou
+# @Mail    : rhyszhou99@gmail.com
+from __future__ import print_function
+import os
+import torch
+import argparse
+import torch.utils.data as data
+from data import cfg_mnet, cfg_re50, cfg_mnetv3
+from models.retinaface import RetinaFace
+
+parser = argparse.ArgumentParser(description='Retinaface Training')
+parser.add_argument('--training_dataset', default='./data/widerface/train/label.txt', help='Training dataset directory')
+parser.add_argument('--network', default='mobile0.25', help='Backbone network mobile0.25 or resnet50')
+parser.add_argument('--num_workers', default=4, type=int, help='Number of workers used in dataloading')
+parser.add_argument('--lr', '--learning-rate', default=1e-3, type=float, help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+parser.add_argument('--resume_net', default=None, help='resume net for retraining')
+parser.add_argument('--resume_epoch', default=0, type=int, help='resume iter for retraining')
+parser.add_argument('--weight_decay', default=5e-4, type=float, help='Weight decay for SGD')
+parser.add_argument('--gamma', default=0.1, type=float, help='Gamma update for SGD')
+parser.add_argument('--save_folder', default='./weights/', help='Location to save checkpoint models')
+
+args = parser.parse_args()
+
+if not os.path.exists(args.save_folder):
+    os.mkdir(args.save_folder)
+cfg = None
+if args.network == "mobile0.25":
+    cfg = cfg_mnet
+elif args.network == "resnet50":
+    cfg = cfg_re50
+elif args.network == "MobileNetV3":
+    cfg = cfg_mnetv3
+
+rgb_mean = (104, 117, 123) # bgr order
+num_classes = 2
+img_dim = cfg['image_size']
+num_gpu = cfg['ngpu']
+batch_size = cfg['batch_size']
+max_epoch = cfg['epoch']
+gpu_train = cfg['gpu_train']
+
+num_workers = args.num_workers
+momentum = args.momentum
+weight_decay = args.weight_decay
+initial_lr = args.lr
+gamma = args.gamma
+training_dataset = args.training_dataset
+save_folder = args.save_folder
+
+net = RetinaFace(cfg=cfg)
+device = torch.device("cpu")
+net = net.to(device)
+from torchsummaryX import summary
+input = torch.randn([10,3,128,96])
+out = net(input)
+summary(net, input)
